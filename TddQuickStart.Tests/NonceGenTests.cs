1d60576ae796b7e5aeb64cbc06b286ebd2cf0ca3
Ethan Nelson <ethan.charles@gmail.com>
2016-03-14 20:11:35 -0700

    simple fact test to ensure test framework is running

--- TddQuickStart.Tests/NonceGenTests.cs
+++ TddQuickStart.Tests/NonceGenTests.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Ploeh.AutoFixture;
+using Ploeh.AutoFixture.AutoMoq;
+using Ploeh.AutoFixture.Xunit2;
+using Xunit;
 
 namespace TddQuickStart.Tests
 {
     public class NonceGenTests
     {
+        [Fact]
+        public void XunitIsFunctional()
+        {
+            Assert.True(true);
+        }
     }
 }
